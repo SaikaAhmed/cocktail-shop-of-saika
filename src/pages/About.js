dba301cc1e56e5f166ff1e58616ab92911e2eb56
Your Name <saikaaridii.com>
2022-07-08 00:10:46 +0600

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        We provide best cocktail drinks in our town. You can easily take this drinks from our restaurant. Also you can share your feelings or thoughts about our service.
+      So, Enjoy your time with our cocktail!
+      </p>
+    </section>
+  );
+}
